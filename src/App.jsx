e0be8a066394449aa216c4504eb06c6146f41827
GuilhermeFinster <guilhermefinsanibal@gmail.com>
2025-06-08 12:02:33 -0300

    Alterado o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PostPage from './pages/PostPage';
 import DadosPage from './pages/DadosPage';
 
@@ -6,6 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Redireciona da rota "/" para "/post" */}
+        <Route path="/" element={<Navigate to="/post" />} />
         <Route path="/post" element={<PostPage />} />
         <Route path="/dados/:id" element={<DadosPage />} />
       </Routes>
